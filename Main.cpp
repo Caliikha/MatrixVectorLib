9451d3470e2016580b2270129e0312fbcd2af618
Caliikha <79475430+Caliikha@users.noreply.github.com>
2021-02-22 23:04:46 +0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -2,38 +2,38 @@
 #include "Matrix2x2.cpp"
 #include "Matrix4x4.cpp"
 #include "Vector4_Temp.cpp"
-//#include <chrono>
+//#include <chrono> 
 #define PI 3.1415926535
 
 int main()
 {
 	vector4 vctr = { 1, 2, 3, 4 };
 
-	Matrix4x4 Xmtrx = {
+	Matrix4x4 Xmtrx = { // Rotation matrix about X axis
 		1,		0,		0,		0,
 		0, cos(PI/2), 		-sin(PI/2),		0,
 		0, sin(PI/2), 		cos(PI/2),		0,
 		0,		0,		0,		1
 	};
-	Matrix4x4 Ymtrx = {
+	Matrix4x4 Ymtrx = { // Rotation matrix about Y axis
 		cos(PI / 2), 0, sin(PI / 2), 			0,
 		0,		1,		0,		0,
 		-sin(PI / 2), 0, cos(PI / 2), 			0,
 		0,		0,		0,		1
 	};
-	Matrix4x4 Zmtrx = {
+	Matrix4x4 Zmtrx = { // Rotation matrix about Z axis
 		cos(PI / 2), -sin(PI / 2), 	0, 		0,
 		sin(PI / 2), cos(PI / 2), 	0, 		0,
 		0,		0,		1,		0,
 		0,		0,		0,		1
 	};
-	Matrix4x4 Translatemtrx = {
+	Matrix4x4 Translatemtrx = { // Translation matrix
 		1,	0,	0,	10,
 		0,	1,	0,	10,
 		0,	0,	1,	10,
 		0,	0,	0,	1
 	};
-	Matrix4x4 Scalemtrx = {
+	Matrix4x4 Scalemtrx = { // Scale matrix
 		2,	0,	0,	1,
 		0,	2,	0,	1,
 		0,	0,	2,	1,
