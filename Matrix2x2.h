55b61fddb9abd797d7c4da9297eaea587bc0f49c
Zaliikha <79475430+Caliikha@users.noreply.github.com>
2021-08-11 13:40:01 +0400

    Added -= operator overload

--- Matrix2x2.h
+++ Matrix2x2.h
@@ -180,4 +180,14 @@ public:
 	Matrix2x2 operator-(const Matrix2x2& right) {
 		return subtract(right);
 	}
+	    
+	Matrix2x2& operator-=(const Matrix2x2& right) {
+		Matrix2x2 resultMtrx = subtract(right);
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 2; j++) {
+				matrix[i][j] = resultMtrx.matrix[i][j];
+			}
+		}
+		return *this;
+	}
 };
