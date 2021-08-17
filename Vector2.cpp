750e2466cb12379191344eb426e45aa57fc30051
Zaliikha <79475430+Caliikha@users.noreply.github.com>
2021-08-17 12:03:25 +0400

    Small edits to includes

--- Vector2.cpp
+++ Vector2.cpp
@@ -1,7 +1,7 @@
 // implementation file for Vector2.h
 #include "Vector2.h"
 #include "Vector3.h"
-#include "../MatrixFiles/Matrix2x2.h"
+#include "Matrix2x2.h"
 #include <cmath>
 #define PI 3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342
 
