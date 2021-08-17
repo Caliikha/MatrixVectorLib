b010d035482ffa580a4e8be2da1f1a03e814055d
Zaliikha <79475430+Caliikha@users.noreply.github.com>
2021-08-17 12:02:20 +0400

    Adding Matrix4x4.cpp implementation file (File restructure)

---
+++ Matrix4x4.cpp
@@ -0,0 +1,164 @@
+#include "Matrix4x4.h"
+#define PI 3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342
+
+Matrix4x4 Matrix4x4::identity() {
+    Matrix4x4 resultMtrx = {};
+    resultMtrx.matrix[0][0] = 1;
+    resultMtrx.matrix[1][1] = 1;
+    resultMtrx.matrix[2][2] = 1;
+    resultMtrx.matrix[3][3] = 1;
+
+    return resultMtrx;
+}
+
+void Matrix4x4::printmtrx() {
+    for (int i = 0; i < Rows; i++) {
+        for (int j = 0; j < Cols; j++) {
+            std::cout << matrix[i][j] << ' ';
+        }
+        std::cout << '\n';
+    }
+    std::cout << '\n';
+}
+
+//template <typename TYPE> // Templated functions must reside in header file
+//void Matrix4x4::showResult(TYPE input){ std::cout << input << '\n'; }
+
+void Matrix4x4::showResult(Matrix4x4 inputmtrx) {
+    for (int i = 0; i < Rows; i++) {
+        for (int j = 0; j < Cols; j++) {
+            std::cout << inputmtrx.matrix[i][j] << ' ';
+        }
+        std::cout << '\n';
+    }
+    std::cout << '\n';
+}
+
+void Matrix4x4::input(Matrix4x4 inputmtrx) {
+    for (int i = 0; i < Rows; i++) {
+        for (int j = 0; j < Cols; j++) {
+            std::cin >> inputmtrx.matrix[i][j];
+        }
+    }
+}
+
+//	float Matrix4x4::determinant() { 
+// TODO: Complete when 3x3 determinant exists, using cofactor method or some dynamic matrix alternative
+//	}
+
+//	Matrix4x4 Matrix4x4::inverse() { // not fully developed, incorrect values will be returned
+// TODO:Complete when determinant function exists 
+//	}
+
+Matrix4x4 Matrix4x4::transpose(){
+    Matrix4x4 resultMtrx;
+
+    for (int i = 0; i < Rows; i++){
+        for (int j = 0; j < Cols; j++){
+            resultMtrx.matrix[j][i] = matrix[i][j];
+        }
+    }
+
+    return resultMtrx;
+}
+
+float Matrix4x4::trace() {
+    return (matrix[0][0] + matrix[1][1] + matrix[2][2] + matrix[3][3]);
+}
+
+Matrix4x4 Matrix4x4::scale(float scale) {
+    Matrix4x4 resultMtrx;
+
+    for (int i = 0; i < Rows; i++) {
+        for (int j = 0; j < Cols; j++) {
+            resultMtrx.matrix[i][j] = (scale * matrix[i][j]);
+        }
+    }
+
+    return resultMtrx;
+}
+
+Matrix4x4 Matrix4x4::multiply(Matrix4x4 inputmtrx) {
+    Matrix4x4 resultMtrx = {};
+
+    for (int i = 0; i < Rows; i++){
+        for (int j = 0; j < Cols; j++){
+            for (int k = 0; k < Rows; k++){
+                resultMtrx.matrix[i][j] += matrix[i][k] * inputmtrx.matrix[k][j];
+            }
+        }
+    }
+
+    return resultMtrx;
+}
+
+//	Matrix4x4 Matrix4x4::divide(Matrix4x4 inputmtrx) {
+// TODO:Complete when determinant and inverse function exists 
+
+//      return multiply(inputmtrx.inverse());
+//	}
+
+Matrix4x4 Matrix4x4::add(Matrix4x4 inputmtrx) {
+    Matrix4x4 resultMtrx;
+    for (int i = 0; i < Rows; i++) {
+        for (int j = 0; j < Cols; j++) {
+            resultMtrx.matrix[i][j] = { matrix[i][j] + inputmtrx.matrix[i][j] };
+        }
+    }
+
+    return resultMtrx;
+}
+
+Matrix4x4 Matrix4x4::subtract(Matrix4x4 inputmtrx) {
+    Matrix4x4 resultMtrx = { };
+
+    for (int i = 0; i < Rows; i++) {
+        for (int j = 0; j < Cols; j++) {
+            resultMtrx.matrix[i][j] = { matrix[i][j] - inputmtrx.matrix[i][j] };
+        }
+    }
+
+    return resultMtrx;
+}
+
+Matrix4x4 Matrix4x4::operator*(const Matrix4x4& right) {
+    return multiply(right);
+}
+
+Matrix4x4& Matrix4x4::operator*=(const Matrix4x4& right) {
+    Matrix4x4 resultMtrx = multiply(right);
+    for (int i = 0; i < Rows; i++) {
+        for (int j = 0; j < Cols; j++) {
+            matrix[i][j] = resultMtrx.matrix[i][j];
+        }
+    }
+    return *this;
+}
+
+Matrix4x4 Matrix4x4::operator+(const Matrix4x4& right) {
+    return add(right);
+}
+
+Matrix4x4& Matrix4x4::operator+=(const Matrix4x4& right) {
+    Matrix4x4 resultMtrx = add(right);
+    for (int i = 0; i < Rows; i++) {
+        for (int j = 0; j < Cols; j++) {
+            matrix[i][j] = resultMtrx.matrix[i][j];
+        }
+    }
+    return *this;
+}
+
+Matrix4x4 Matrix4x4::operator-(const Matrix4x4& right) {
+    return subtract(right);
+}
+
+Matrix4x4& Matrix4x4::operator-=(const Matrix4x4& right) {
+    Matrix4x4 resultMtrx = subtract(right);
+    for (int i = 0; i < Rows; i++) {
+        for (int j = 0; j < Cols; j++) {
+            matrix[i][j] = resultMtrx.matrix[i][j];
+        }
+    }
+    return *this;
+}
